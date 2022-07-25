606ac2fce4b9a3eb9e4f97febb2b7d06a6867f2d
zakzakst <e1433104@gmail.com>
2022-07-25 09:49:18 +0900

    ヘッダー作成

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 // Option 2
 //
@@ -16,5 +16,5 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import '../../node_modules/bootstrap/js/dist/util/index.js';
-import '../../node_modules/bootstrap/js/dist/modal.js';
+// import '../../node_modules/bootstrap/js/dist/util/index.js';
+// import '../../node_modules/bootstrap/js/dist/modal.js';
